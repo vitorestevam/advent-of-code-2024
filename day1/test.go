e57ed2fbcce44267695e9c4ee3cbad677d43e39e
Vitor Estevam <vitorestevam02@gmail.com>
2024-12-04 23:17:31 -0300

    reorganizing tests

--- day1/test.go
+++ day1/test.go
@@ -1,31 +1,34 @@
-package adventofcode2024
+package day1
 
 import (
 	"testing"
 
-	day1 "github.com.br/vitorestevam/adventofcode2024/day1"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDay1(t *testing.T) {
+func TestPart1(t *testing.T) {
 	t.Run("part 1 - test input", func(t *testing.T) {
 		expected := 11
-		output := day1.Part1("day1/input_test.txt")
+		output := Part1("day1/input_test.txt")
 		assert.Equal(t, expected, output)
 	})
 	t.Run("part 1 - real input", func(t *testing.T) {
 		expected := 1319616
-		output := day1.Part1("day1/input.txt")
+		output := Part1("day1/input.txt")
 		assert.Equal(t, expected, output)
 	})
+}
+
+func TestPart2(t *testing.T) {
+
 	t.Run("part2 - test input", func(t *testing.T) {
 		expected := 31
-		output := day1.Part2("day1/input_test.txt")
+		output := Part2("day1/input_test.txt")
 		assert.Equal(t, expected, output)
 	})
 	t.Run("part2 - real input", func(t *testing.T) {
 		expected := 27267728
-		output := day1.Part2("day1/input.txt")
+		output := Part2("day1/input.txt")
 		assert.Equal(t, expected, output)
 	})
 }
